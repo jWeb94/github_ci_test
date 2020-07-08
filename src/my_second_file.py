27eb9b57e24c2240d823816f4f8ccb5ce8e5029c
Jens <jweber1094@gmail.com>
2020-07-08 07:24:35 +0200

    bugfix pydocstyle

--- src/my_second_file.py
+++ src/my_second_file.py
@@ -1,10 +1,8 @@
-"""
-This is the utils library for the test of the github ci-system
-"""
+
 
 def my_function_1(a, b):
     return (a + b)
 
 
 def my_function_2(a, b, c):
-    return a + b + c 
+    return a + b + c
